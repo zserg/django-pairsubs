cb9d72a4260224425afb19c0815f26013097819e
Sergey Zadyabin <zsergt3@gmail.com>
2018-08-05 18:24:19 +0300

    bootstrap tuning (search form)

--- pairsubs/forms.py
+++ pairsubs/forms.py
@@ -1,6 +1,25 @@
 from django import forms
 
 class SubSearchForm(forms.Form):
-    imdb = forms.CharField(label='IMDB', max_length=256)
-    lang1 = forms.CharField(label='Lang #1', max_length=3)
-    lang2 = forms.CharField(label='Lang #2', max_length=3)
+    w = forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'rus'})
+    imdb = forms.CharField(
+         label='IMDb link',
+         widget = forms.TextInput(
+           attrs={'class': 'form-control',
+                  'placeholder': 'https://www.imdb.com/title/tt0583539/?ref_=ttep_ep4'}),
+	)
+    lang1 = forms.CharField(
+     	 label='First language',
+         max_length=3,
+         widget = forms.TextInput(
+           attrs={'class': 'form-control',
+                  'placeholder': 'rus'}),
+	)
+
+    lang2 = forms.CharField(
+     	 label='Second language',
+         max_length=3,
+         widget = forms.TextInput(
+           attrs={'class': 'form-control',
+                  'placeholder': 'eng'}),
+	)
